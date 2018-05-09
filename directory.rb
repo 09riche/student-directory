b0a3f48206f3abe9f61a32d80d6a0c69a845329b
Nick <nicholasjohnrichardson@gmail.com>
2018-05-09 12:31:03 +0100

    Added list by cohort functionality

--- directory.rb
+++ directory.rb
@@ -1,4 +1,6 @@
 @students = []
+
+# --------------loading and saving files ----------
 def try_load_students
   filename = ARGV.first
   return if filename.nil?
@@ -10,6 +12,7 @@ def try_load_students
     exit
   end
 end
+
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
@@ -18,17 +21,9 @@ def load_students(filename = "students.csv")
   end
   file.close
 end
-def interactive_menu
-  puts `clear`
-  loop do
-    print_menu
-    process(STDIN.gets.chomp)
-  end
-end
+
 def save_students
-  # open the file for writing
   file = File.open("students.csv", "w")
-  # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
@@ -36,6 +31,16 @@ def save_students
   end
   file.close
 end
+
+#------------------ Menu and menu input processing -------
+def interactive_menu
+  puts `clear`
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
 def process(selection)
   case selection
     when "1"
@@ -46,25 +51,15 @@ def process(selection)
       save_students
     when "4"
       load_students
+    when "5"
+      list_by_cohort
     when "9"
       exit
     else
       puts "I don't know what you meant, try again"
   end
 end
-def print_menu
-  puts "\nMENU"
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
-end
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
+
 def input_students
   puts "Please enter the name of the student and their cohort. To finish, hit return twice."
   name, cohort = "Name", "Cohort"
@@ -75,25 +70,51 @@ def input_students
     puts "Cohort?"
     cohort = STDIN.gets.chomp.to_sym
     @students << {name: name, cohort: cohort}
-    if @students.length == 1
-      puts "\nNow we have #{@students.count} student"
-    else
-      puts "\nNow we have #{@students.count} students"
-    end
+    student_counter
   end
   @students
 end
+
+
+# ---------- printing things ----------
+def print_menu
+  puts "\nMENU"
+  puts "1. Input the students"
+  puts "2. List the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "5. List the students by cohort"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  student_counter
+end
+
+def list_by_cohort
+  puts "Which cohort would you like to list by?"
+  cohort = gets.chomp.to_sym
+  puts "Students in #{cohort} Cohort:\n\n"
+  @students.each_with_index do |student, i|
+    puts "#{i + 1}. #{student[:name]}" if student.key(cohort)
+  end
+end
+
 def print_header
   puts `clear`
   puts "The students of Villains Academy"
   puts "-------------"
 end
+
 def print_students_list
   @students.each_with_index do |student, i|
     puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} Cohort)"
   end
 end
-def print_footer
+
+def student_counter
   if @students.count == 1
     puts "\nWe have #{@students.count} villainous student.\n"
   else
